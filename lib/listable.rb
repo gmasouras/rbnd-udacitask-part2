83ccf7e835f77ddd75f61cb058d8a0edaa444c48
gmasouras <masourasgeorge@gmail.com>
2016-05-05 22:12:50 +0200

    Listable module with three format methods completed

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,4 +3,19 @@ module Listable
   def format_description(description)
 	"#{description}".ljust(30)  	
   end
+  def format_date(start_date, end_date)
+  	dates = start_date.strftime("%D") if start_date
+    dates << " -- " + end_date.strftime("%D") if end_date
+    dates = "N/A dates" if !dates
+    return dates
+  end
+  def format_priority(priority)
+  	case priority
+	  	when "high" then " ⇧"
+	  	when "medium" then " ⇨" 
+	  	when "low" then " ⇩"
+  	else
+  		""
+  	end	
+  end
 end
